44eaaccd0e6f10e0b4a4e50f54b831921276967f
Jesper Jensen <jesper@slashwin.dk>
2021-01-22 23:25:58 +0100

    Change windows key to menu(compose) key.

--- keyboards/massdrop/alt/keymaps/delusional/keymap.c
+++ keyboards/massdrop/alt/keymaps/delusional/keymap.c
@@ -42,7 +42,7 @@ const uint32_t PROGMEM colmap[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,          KC_TRNS, KC_TRNS, \
         KC_TRNS, KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS  \
     ),
-    [4] = LAYOUT(
+    [3] = LAYOUT(
         KC_NO,   COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, \
         COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, \
         COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_TRN, COL_NUM, COL_NUM, COL_NUM, COL_NUM, COL_TRN, COL_TRN,          COL_TRN, COL_HOD, \
@@ -90,15 +90,13 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
 #define TG_NKRO MAGIC_TOGGLE_NKRO //Toggle 6KRO / NKRO mode
 
-keymap_config_t keymap_config;
-
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
-        MO(4),          KC_1,         KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,        KC_EQL,  KC_BSPC, KC_DEL,  \
+        MO(3),          KC_1,         KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS,        KC_EQL,  KC_BSPC, KC_DEL,  \
         KC_TAB,         KC_Q,         KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC,        KC_RBRC, KC_BSLS, KC_HOME, \
         KC_ESC,         KC_A,         KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                 KC_ENT,  KC_PGUP, \
         KC_LSFT,        KC_Z,         KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSHIFT,      KC_UP,   KC_PGDN, \
-        KC_LCTL,        OSM(MOD_LGUI),KC_LALT,                            KC_SPC,                             KC_RALT, OSM(MOD_RGUI),  KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL,        KC_LGUI,      KC_LALT,                            KC_SPC,                             KC_RALT, KC_APP,         KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     // Disable half the keyboard depending on which shift key we've pressed
     [1] = LAYOUT(
@@ -116,7 +114,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TRNS,        KC_TRNS, KC_TRNS,                            KC_TRNS,                            KC_TRNS, KC_TRNS,        KC_TRNS, KC_TRNS, KC_TRNS  \
     ),
     // FN layer
-    [4] = LAYOUT(
+    [3] = LAYOUT(
         KC_TRNS,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,         KC_F12,  KC_DEL,  KC_MUTE, \
         KC_TRNS,        RGB_SPD, RGB_VAI, RGB_SPI, RGB_HUI, RGB_SAI, KC_GRV,  U_T_AUTO,U_T_AGCR,KC_TRNS, KC_PSCR, KC_SLCK,        KC_PAUS, KC_TRNS, KC_END, \
         KC_CAPS,        RGB_RMOD,RGB_VAD, RGB_MOD, RGB_HUD, RGB_SAD, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_TRNS, KC_TRNS,                 KC_TRNS, KC_VOLU, \
